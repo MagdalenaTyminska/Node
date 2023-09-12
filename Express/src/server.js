77423592613507ea35cd389c3153ce5fcb85b3a0
Magdalena Bonk <106923695+MagdaBonk@users.noreply.github.com>
2023-09-12 20:53:40 +0200

    server

---
+++ Express/src/server.js
@@ -0,0 +1,19 @@
+import express, { request } from "express";
+import colors from "colors";
+
+const app = express();
+
+app.get("/", (request, response) => {
+  response.send("Hello there");
+});
+
+app.get("/hi", (request, response) => {
+  response.send("<h1 style='color:dodgerblue'>Hi!</h1>");
+  console.log(new Date());
+});
+
+// najczęściej wykorzystywany port lub 8080
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(colors.cyan(`[server] Server running on port ${PORT}`));
+});
