e3c6664d8984c38721814dba151be20f261db84e
Magdalena Bonk <106923695+MagdaBonk@users.noreply.github.com>
2023-09-13 16:29:21 +0200

    middlewares

--- Express/src/middlewares.js
+++ Express/src/middlewares.js
@@ -3,23 +3,47 @@ import colors from "colors";
 
 const app = express();
 
-let visits = 0;
+//możemy podać ścieżkę zamiast funkcji, możemy zostawić app.use albo wyciągnąc do osobnych funkcji jak poniżej
+// app.use((req, res, next) => {
+//   const { originalUrl } = req;
+//   visits[originalUrl] = visits[originalUrl] ? ++visits[originalUrl] : 1;
+//   console.log(colors.magenta(`Visits: ${visits[originalUrl]}`));
+//   next();
+// });
 
-//możemy podać ścieżkę zamiast funkcji
-app.use((req, res, next) => {
+const logger = (req, res, next) => {
   const { method, originalUrl } = req;
   const date = new Date().toLocaleString();
   console.log(colors.yellow(`[${method}] ${date} - ${originalUrl}`));
   next();
-});
+};
+
+const visitsCounter = () => {
+  const visits = {};
+  return (req, res, next) => {
+    const { originalUrl } = req;
+    visits[originalUrl] = visits[originalUrl] ? ++visits[originalUrl] : 1;
+    console.log(colors.magenta(`Visits: ${visits[originalUrl]}`));
+    next();
+  };
+};
+
+app.use(logger, visitsCounter());
 
 app.get("/", (req, res) => {
-  res.send("Hello there");
+  return res.send("Hello there");
 });
 
 app.get("/hi", (req, res) => {
-  res.send("<h1 style='color:dodgerblue'>Hi!</h1>");
+  return res.send("<h1 style='color:dodgerblue'>Hi!</h1>");
   console.log(new Date());
+});
+
+app.use((req, res) => {
+  const notFoundMessage = "Ooopsie! You got lost? There is no such path!";
+  return res
+    .status(404)
+    .send(`<h1 style='color:crimson'>${notFoundMessage}</h1>`);
 });
 
 // najczęściej wykorzystywany port lub 8080
